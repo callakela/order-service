21b4b8229e1ad5b533c5751e064b77eec0e4bcf4
Amit Kumar Akela <amitk.akela@impetus.co.in>
2017-12-29 12:03:15 +0530

    commit

--- src/main/java/com/vs/os/api/OrderListApiController.java
+++ src/main/java/com/vs/os/api/OrderListApiController.java
@@ -1,24 +1,16 @@
 package com.vs.os.api;
 
-import io.swagger.annotations.*;
+import io.swagger.annotations.ApiParam;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.vs.os.model.Error;
 import com.vs.os.model.Order;
 
-import java.util.List;
 
 
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2016-09-17T10:01:03.258Z")
 
 @Controller
 public class OrderListApiController implements OrderListApi {
--- src/main/java/com/vs/os/api/PlaceOrderApi.java
+++ src/main/java/com/vs/os/api/PlaceOrderApi.java
@@ -1,23 +1,18 @@
 package com.vs.os.api;
 
-import io.swagger.annotations.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.vs.os.model.Error;
 import com.vs.os.model.Order;
 
-import java.util.List;
-
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2016-09-17T10:01:03.258Z")
 
 @Api(value = "placeOrder", description = "the placeOrder API")
 public interface PlaceOrderApi {
